ef87ea59c4def261d0215c841d380a8d4b3523d7
Munawar Ali <munawar.ali@Enterprise64.com>
2024-05-24 12:17:21 +0500

    WeatherChange

--- PracticeJenkinsAndWebApi/Controllers/WeatherForecastController.cs
+++ PracticeJenkinsAndWebApi/Controllers/WeatherForecastController.cs
@@ -24,7 +24,7 @@ namespace PracticeJenkinsAndWebApi.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = 123,
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
